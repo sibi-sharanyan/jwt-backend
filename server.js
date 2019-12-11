5f1ca7395427e5eea51cc79f4b866a0e7a26a8a0
sibi sharanyan <sibisharanyanit@gmail.com>
2019-12-11 17:26:14 +0530

    version that stores token in db

---
+++ server.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const bcryptjs = require('bcryptjs');
+const _ = require('lodash');
+
+const User = require('./user');
+
+var app = express();
+app.use(bodyParser.json());
+
+
+mongoose.connect(
+    process.env.DATABASEURL  || require('./config').mongourl  ,
+      { useNewUrlParser: true , useUnifiedTopology: true , useCreateIndex: true}
+      );
+
+      
+//Middleware to check if user logged in
+var authenticate = (req , res ,next) => {
+    var token = req.header('x-auth');
+
+    User.findByToken(token).then((user) => {
+        if(!user) {
+         return Promise.reject({
+             message : 'No such user'
+         })
+        }
+        
+        req.user = user;
+        req.token = token;
+        next();
+    }).catch((e) => {
+        res.status(401).send(e);
+    })
+
+
+}
+
+
+
+
+
+//Sign Up user
+app.post('/users' , ( req , res) => {
+    
+    var body = _.pick(req.body , ['email' , 'password']);
+    var user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth' , token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+})
+
+
+//Login user
+app.post('/users/login' , ( req , res) => {
+    
+    var body = _.pick(req.body , ['email' , 'password']);
+    
+    User.login(body.email , body.password).then(async (user) => {
+       var token = await user.generateAuthToken();
+        res.header('x-auth' , token).send(user);
+    }).catch((e) => {
+        res.status(401).send(e);
+    })
+
+})
+
+//Route to check authentication
+app.get('/home' , authenticate , (req , res) => {
+    res.send('Hello World');
+})
+
+
+app.listen(8000 , () => {
+    console.log('Server Started');
+})
